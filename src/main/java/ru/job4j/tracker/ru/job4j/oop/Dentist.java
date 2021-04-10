b0926094179f564f68b49ed59fd890176e7a5eb0
Zuxelator <zuxel87@mail.ru>
2021-04-10 19:44:39 +0400

    3. Реализация профессий в коде [#497109]

---
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/Dentist.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.ru.job4j.oop;
+
+public class Dentist extends Doctor {
+    private int toothDeleted;
+
+    public Dentist(String name, String surname, String education, String birthday, int toothDeleted) {
+        super(name, surname, education, birthday);
+        this.toothDeleted = toothDeleted;
+    }
+
+    public int getToothDeleted() {
+        return toothDeleted;
+    }
+
+    public void deleteTooth() {
+
+    }
+}
