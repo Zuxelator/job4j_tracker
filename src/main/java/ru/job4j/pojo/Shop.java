016c64db6ea967c75e4581d2af7703ccf3441f08
Zuxelator <zuxel87@mail.ru>
2021-04-11 14:22:31 +0400

    3.1. Массив с пустыми ячейками [#497114]

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+public class Shop {
+    public static int indexOfNull(Product[] products) {
+        int res = -1;
+        for (int index = 0; index < products.length; index++) {
+            if(products[index] == null) {
+                res = index;
+                break;
+            }
+        }
+        return res;
+    }
+}
