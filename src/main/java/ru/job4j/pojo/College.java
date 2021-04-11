fd92041d5e8270bbceab506f7f36950a3e109398
Zuxelator <zuxel87@mail.ru>
2021-04-11 13:44:35 +0400

    2. Модель данных. [#497111]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.time.LocalDate;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Иванов Пётр Сергеевич");
+        student.setGroup("1401Б");
+        student.setDateOfAdmission(LocalDate.of(2020, 9, 10));
+        System.out.println("ФИО : " + student.getFio() + System.lineSeparator() +
+                           "Группа : " + student.getGroup() + System.lineSeparator() +
+                           "Дата поступления : " + student.getDateOfAdmission());
+    }
+}
