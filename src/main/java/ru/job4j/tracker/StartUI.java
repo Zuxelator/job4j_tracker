1a333bf92e59a62ce62d659bbac40947059cab06
Zuxelator <zuxel87@mail.ru>
2021-04-12 01:24:36 +0400

    9. toString [#497119]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,10 +2,9 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        Item item = new Item();
-        item.setName("Bug");
-        tracker.add(item);
-        System.out.println(tracker.findById(1).getName());
+       Item item = new Item();
+       item.setId(4);
+       item.setName("Bug");
+       System.out.println(item);
     }
 }
