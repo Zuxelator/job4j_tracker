351ce233937eb72a70d6056fcabb3ddb88f1f98a
Zuxelator <zuxel87@mail.ru>
2021-04-12 17:14:41 +0400

    7. Метод удаления заявки Tracker.delete [#497116]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -64,6 +64,7 @@ public class Tracker {
         boolean rsl = index != - 1;
         if(rsl) {
             System.arraycopy(items, index + 1, items, index, size - 1 - index);
+            items[size - 1] = null;
             size--;
         }
         return rsl;
