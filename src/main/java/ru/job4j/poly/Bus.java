db8ddb5d1ef974b9a2a65ee9f506434c358ffc14
Zuxelator <zuxel87@mail.ru>
2021-04-12 23:39:43 +0400

    3. Интерфейс Input [#497043]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void go() {
+        System.out.println("Автобус едет");
+    }
+
+    @Override
+    public void passangers(int passAmount) {
+        System.out.println("Пассажиров в автобусе: " + passAmount);
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        return fuel * 50;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void go();
+
+    void passangers(int passAmount);
+
+    double refuel(double fuel);
+
+}
