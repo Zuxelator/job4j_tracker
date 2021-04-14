bd368e8e05b01f99c1510aabe0cda709474e908e
Zuxelator <zuxel87@mail.ru>
2021-04-14 17:34:09 +0400

    9. Написать тесты на StartUI. [#497054]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,66 +1,6 @@
 package ru.job4j.tracker;
 
 public class StartUI {
-    public static void createItem(Input input, Tracker tracker) {
-        System.out.println("=== Create a new Item ====");
-        String name = input.askStr("Enter name: ");
-        Item item = new Item(name);
-        tracker.add(item);
-    }
-
-    public static void  showAllItems(Tracker tracker) {
-        System.out.println("=== Show all items ====");
-        Item[] arr = tracker.findAll();
-        for(Item item: arr) {
-            System.out.println(item);
-        }
-    }
-
-    public static void replaceItem(Input input, Tracker tracker) {
-        System.out.println("=== Edit item ====");
-        int itemId = input.askInt("Enter ID ");
-        String name = input.askStr("Enter name of new Item ");
-        Item item = new Item(name);
-        if(tracker.replace(itemId,item)) {
-            System.out.println("Item changed successfully");
-        } else {
-            System.out.println("Error: An element with this ID was not found.");
-        }
-    }
-
-    public static void deleteItem(Input input, Tracker tracker) {
-        System.out.println("=== Delete item ====");
-        int itemId = input.askInt("Enter ID ");
-        if(tracker.delete(itemId)) {
-            System.out.println("Item deleted successfully");
-        } else {
-            System.out.println("Error: An item with this ID was not found.");
-        }
-    }
-
-    public static void findItemByID(Input input, Tracker tracker) {
-        System.out.println("=== Find item by Id ===");
-        int itemId = input.askInt("Enter ID ");
-        Item item = tracker.findById(itemId);
-        if (item != null) {
-            System.out.println(item);
-        } else {
-            System.out.println("The item with this id was not found");
-        }
-    }
-
-    public static void findItemByName(Input input, Tracker tracker) {
-        System.out.println("=== Find items by name ===");
-        String name = input.askStr("Enter name of item ");
-        Item[] arr = tracker.findByName(name);
-        if(arr.length > 0) {
-            for (Item item : arr) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Items with this name not found");
-        }
-    }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
