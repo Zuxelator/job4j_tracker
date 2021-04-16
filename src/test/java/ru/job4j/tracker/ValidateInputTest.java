27531ee960ce0b9935417e7c62649626b355367f
Zuxelator <zuxel87@mail.ru>
2021-04-16 18:54:17 +0400

    2.1. Тест на ValidateInput [#497067]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(3));
+    }
+    @Test
+    public void when3TimesValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"3", "4", "5"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        selected = input.askInt("Enter menu:");
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(5));
+    }
+
+    @Test
+    public void whenMinusInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-3));
+    }
+}
