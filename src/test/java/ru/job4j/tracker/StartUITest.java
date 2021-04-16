b6cb9fc6e2e99d4519cfc872ed57462f1492b2e5
Zuxelator <zuxel87@mail.ru>
2021-04-16 12:50:09 +0400

    1.1. Тесты на StartUI. [#497066]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -133,4 +133,26 @@ public class StartUITest {
                 "0. Find item by Id" + System.lineSeparator() +
                 "1. Exit Program" + System.lineSeparator() ));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {/* Пункты меню: неверный, верный. */ "10", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu." + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu." + ln
+                        + "0. Exit Program" + ln
+                )
+        );
+    }
 }
