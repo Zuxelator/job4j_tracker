1e67e7de60ab271d740df1cac097b3520d21e6bd
Zuxelator <zuxel87@mail.ru>
2021-05-10 17:52:39 +0400

    2.2. Лямбда блок [#496845]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,19 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList("asd", "ss", "ssdsdas", "dasd"));
+        Comparator<String> descComp = (left, right) -> {
+            System.out.println("compare - " + right + " : " + left);
+            return right.length() - left.length();
+        };
+        System.out.println(arr);
+        Collections.sort(arr, descComp);
+        System.out.println(arr);
+    }
+}
