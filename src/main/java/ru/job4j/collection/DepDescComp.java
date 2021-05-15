c2e956286cf5f31db6e57719b9935d3e40ac1f11
Zuxelator <zuxel87@mail.ru>
2021-05-15 15:24:11 +0400

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#496851]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -6,7 +6,8 @@ public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         String leftFirstElement = left.contains("/") ? left.substring(0, left.indexOf('/')) : left;
-        String rightFirstElement = right.contains("/") ? right.substring(0, right.indexOf('/')) : right;
+        String rightFirstElement =
+                right.contains("/") ? right.substring(0, right.indexOf('/')) : right;
         int rsl = rightFirstElement.compareTo(leftFirstElement);
         if (rsl == 0) {
             return left.compareTo(right);
