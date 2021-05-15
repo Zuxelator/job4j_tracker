3bc8f53e11fbbd670aeb4f5de92f28c8fe7302dd
Zuxelator <zuxel87@mail.ru>
2021-05-16 00:48:15 +0400

    1. Метод .of [#496864]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -13,32 +14,26 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+        Set<Account> expect = Set.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
-                        new Account("142", "Petr Arsentev", "000001")
-                )
-        );
+                        new Account("142", "Petr Arsentev", "000001"));
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 
     @Test
     public void sentDeleteDoubles() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001"),
                 new Account("142", "Petr AAA", "00555")
         );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+        Set<Account> expect = Set.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
-                        new Account("142", "Petr Arsentev", "000001")
-                )
-        );
+                        new Account("142", "Petr Arsentev", "000001"));
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 }
